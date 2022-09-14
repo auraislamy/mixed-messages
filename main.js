4736b40e5f403379b094af440f7b53e3e6cfc557
Aura Islamy <auraislamy08@gmail.com>
2022-09-15 01:31:46 +0800

    add html and css

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 /* Mixed Message Program */
 
+const generateButton = document.getElementById('generate');
+const quotesDisplay = document.getElementById('quotes');
+
+
+
+generateButton.addEventListener('click', () => {
+    const random = Math.floor(Math.random() * 3);
+    const happyMessage = "---Today's Quotes For You---\n\n😊 " + happyQuotes[random] + "\n\n"
+    const sadMessage = `😔 ${sadQuotes[random]}\n\n`;
+    const gratitudeMessage = `😇 ${gratitudeQuotes[random]}\n\n---We hope this quotes is useful for you!✨---`;
+    
+    quotesDisplay.innerText = happyMessage + sadMessage + gratitudeMessage;
+});
+
 /* array of messages */
 
 const happyQuotes = ["You're never too old to chase your dreams.", 
@@ -12,17 +26,5 @@ const gratitudeQuotes = ["Learn to let go. That is the key to happiness.",
 "Be happy for this moment. This moment is your life.",
 "It is not joy that makes us grateful; it is gratitude that makes us joyful."];
 
-/* function that return a random message */
 
-const randMessage = () => {
-    const random = Math.floor(Math.random() * happyQuotes.length);
-    // const quotes = [happyQuotes, sadQuotes, gratitudeQuotes];
-    // const randQuotes = Math.floor(Math.random() * quotes.length);
-    
-    const happyMessage = "---Today's Quotes For You---\n😊 " + happyQuotes[random] + "\n"
-    const sadMessage = `😔 ${sadQuotes[random]}\n`;
-    const gratitudeMessage = `😇 ${gratitudeQuotes[random]}\n---We hope this quotes is useful for you!✨---`;
-    return happyMessage + sadMessage + gratitudeMessage;
-}
 
-console.log(randMessage());
